2e9cd7d0d5659e5e78b7c1669bade114e3a8b396
mikhailkomarov <michaelkomarov1@gmail.com>
2019-03-14 16:41:57 +0300

    first commit wrappers-for-primitives/task.js

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -7,22 +9,47 @@ function calculateMortgage() {
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
     span.textContent = result;
-}
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  let creditTerms = []; // Инициализация пустого массива 
+  for (let i = 0; i < arguments.length - 1; i++) {
+    creditTerms[i] = parseInt(arguments[i]);
+    if (isNaN(arguments[i])) {
+      return `Параметр ${i + 1} содержит не правильное значение ${arguments[i]}`;
+    }  
+  }
+  let S = amount - contribution; // Переменная хранящая сумму, которую необходимо вернуть банку
+  let P = (percent / 100) / 12; // Переменная хранащая процентную ставку по кредиту
+  let currentDate = new Date(); // Переменная хранящая значение текущей даты
+  date = new Date(date); // Переменная хранящая значение даты окончания кредита
+  let n = ((date.getMonth() - currentDate.getMonth()) + (12 * (date.getFullYear() - currentDate.getFullYear()))); // Коли-во месяцев кредита
+  let payment = (S * (P + P / (((1 + P) ** n) - 1))); // Ежемесячный платеж
+  let totalAmount = (payment * n).toFixed(2); // Общая сумма к оплате
+  return totalAmount;
+};
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
-}
+
+// Задание №2
 
 function sayHello() {
     let name = window.personName.value;
     let greeting = getGreeting(name);
     let span = window.helloResult;
     span.textContent = greeting;
-}
+};
+
+// Функция которая проверяет наличие или отсутствие имени 
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
-}
+  if (name == 'null' || name == 'undefined') {
+    console.log('Привет,мир! Меня зовут Аноним');
+    return 'Привет,мир! Меня зовут Аноним'
+  } else if (name == Number(name)) {
+    console.log('Привет,мир! Меня зовут Аноним');
+    return 'Привет,мир! Меня зовут Аноним';
+  } else {
+    console.log(`Привет, мир! Меня зовут ${name}.`);
+    return `Привет, мир! Меня зовут ${name}.`;
+  };
+};
